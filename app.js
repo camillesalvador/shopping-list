add77c66375c93801eff99ba4cc219fdf701221b
Camille Salvador <camille.salvador@outlook.com>
2017-08-16 16:25:14 -0600

    added add, delete, toggle functionalities

--- app.js
+++ app.js
@@ -1,24 +1,100 @@
-//single state objects
 var state = {
-  items: []
+  items: [
+    {
+      displayItem: "apples",
+      checked: false
+    },
+    {
+      displayItem: "oranges",
+      checked: false
+    },
+    {
+      displayItem: "milk",
+      checked: true
+    },
+    {
+      displayItem: "bread",
+      checked: false
+    }
+  ]
 };
 
-//state modification functions
-var addItem = function (state, item) {
-  state.items.push(item);
-};
-
-//render functions
-var renderList = function (state, element) {
-  var itemsHTML = state.items.map(function (item) {
-    return '<li>' + item + '</li>';
+// state management
+function addItem(state, item) {
+  state.items.push({
+    displayItem: item,
+    checked: false
   });
-  element.html(itemsHTML);
-};
+}
 
-// event listeners
-$('#js-shopping-list-form').submit(function(event) {
-  event.preventDefault();
-  addItem(state, $('#shopping-list-entry').val());
-  renderList(state, $('.shopping-list'));
+function toggleItem(state, itemIndex) {
+  state.items[itemIndex].checked = !state.items[itemIndex].checked;
+}
+
+function deleteItem(state, itemIndex) {
+  state.items.splice(itemIndex, 1);
+}
+
+// DOM manipulation
+function renderList(state, element) {
+  var html = state.items.map(function(item, index) {
+    var extra = '';
+    if(item.checked) {
+      extra = ' shopping-item__checked';
+    }
+    var str = '<li id="' + index + '">' +
+      '<span class="shopping-item' + extra + '">' + item.displayItem + '</span>' +
+      '<div class="shopping-item-controls">' +
+        '<button class="shopping-item-toggle">' +
+          '<span class="button-label">check</span>' +
+        '</button>&nbsp;' +
+        '<button class="shopping-item-delete">' +
+          '<span class="button-label">delete</span>' +
+        '</button>' +
+      '</div>' +
+    '</li>';
+
+    return str;
+  });
+  element.html(html);
+}
+
+
+// Event listeners
+function handleAddItem(formElement, newItemIdentifier, listElement) {
+  $(formElement).submit(function(event) {
+    event.preventDefault();
+    addItem(state, $(newItemIdentifier).val());
+    renderList(state, listElement);
+  });
+}
+
+function handleToggleItem(toggleIdentifier, listElement) {
+  $(listElement).on('click', toggleIdentifier, function(event) {
+    event.stopPropagation();
+    var itemIndex = $(event.currentTarget.closest('li')).attr('id');
+    toggleItem(state, itemIndex);
+    renderList(state, listElement);
+  });
+}
+
+function handleDeleteItem(removeIdentifier, listElement) {
+  $(listElement).on('click', removeIdentifier, function(event) {
+    event.stopPropagation();
+    var itemIndex = $(event.currentTarget.closest('li')).attr('id');
+    deleteItem(state, itemIndex);
+    renderList(state, listElement);
+  });
+}
+
+$(function() {
+  var formElement = $('#js-shopping-list-form');
+  var listElement = $('.shopping-list');
+  var newItemIdentifier = '#shopping-list-entry';
+  var removeIdentifier = '.shopping-item-delete';
+  var toggleIdentifier = '.shopping-item-toggle';
+  
+  handleAddItem(formElement, newItemIdentifier, listElement);
+  handleToggleItem(toggleIdentifier, listElement);
+  handleDeleteItem(removeIdentifier, listElement);
 });
